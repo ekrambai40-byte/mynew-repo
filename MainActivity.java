fa4f8d014b6b549d00bea80c5b0e462926648820
ekrambai40-byte <ekrambai40@gmail.com>
2026-09-01 17:42:19 +0600

    MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,5 @@
-package com.example.help;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello from GitHub Actions Build!");
-        setContentView(tv);
+public class MainActivity {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions Java Build!");
     }
 }
